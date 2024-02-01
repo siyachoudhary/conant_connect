0cbf03373b419fc0da47081ba03b5624bd12c026
Siya Choudhary <Siyacutesiya@gmail.com>
2024-01-31 22:24:29 -0600

    Backend layout added but won't run :(

---
+++ server/app/controllers/tutorial.controller.js
@@ -0,0 +1,13 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new Tutorial
+exports.create = (req, res) => {
+  
+};
+
+// Find a single Tutorial with an id
+exports.findOne = (req, res) => {
+  
+};
